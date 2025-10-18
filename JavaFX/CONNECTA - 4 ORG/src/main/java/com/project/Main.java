8cb8cf0b91650e6fd99436f396bbbea031636fd0
laurattt <lauratoro422@gmail.com>
2025-10-19 00:38:10 +0200

    update

--- JavaFX/CONNECTA - 4 ORG/src/main/java/com/project/Main.java
+++ JavaFX/CONNECTA - 4 ORG/src/main/java/com/project/Main.java
@@ -9,12 +9,11 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    final int WINDOW_WIDTH = 800;
-    final int WINDOW_HEIGHT = 600;
+    final int WINDOW_WIDTH = 1400;
+    final int WINDOW_HEIGHT = 900;
 
     public static CtrlConfig ctrlConfig;
     public static CtrlListaJugadores ctrlListPlayers;
-    public static CtrlWait ctlWait;
    
 
     @Override
@@ -25,20 +24,18 @@ public class Main extends Application {
         UtilsViews.parentContainer.setStyle("-fx-font: 14 arial;");
         UtilsViews.addView(getClass(), "ViewConfig", "/assets/viewConfig.fxml"); 
         UtilsViews.addView(getClass(), "ViewListPlayers", "/assets/viewListPlayers.fxml"); 
-        UtilsViews.addView(getClass(), "ViewWait", "/assets/viewWait.fxml");
         // CUENTA REGRESIVA 
         UtilsViews.addView(getClass(), "ViewTablero", "/assets/tableroPartida.fxml"); 
         
         // Controladores
         ctrlConfig = (CtrlConfig) UtilsViews.getController("ViewConfig");
         ctrlListPlayers= (CtrlListaJugadores) UtilsViews.getController("ViewListPlayers");
-        ctlWait = (CtrlWait) UtilsViews.getController("ViewWait");
 
 
         Scene scene = new Scene(UtilsViews.parentContainer);
 
         stage.setScene(scene);
-        stage.setTitle("CONNECTA-4");
+        stage.setTitle("Connecta4-MonicaD-LauraT");
         stage.setWidth(WINDOW_WIDTH);
         stage.setHeight(WINDOW_HEIGHT);
         stage.show();
